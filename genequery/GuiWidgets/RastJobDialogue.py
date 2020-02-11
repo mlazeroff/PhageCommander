7146b6594781ea1a906d643af244166d81367919
Matthew Lazeroff <mdlazeroff@gmail.com>
2020-02-11 06:39:31 -0800

    RastJobDialogue - User/Pass validation

--- genequery/GuiWidgets/RastJobDialogue.py
+++ genequery/GuiWidgets/RastJobDialogue.py
@@ -1,9 +1,12 @@
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
+import webbrowser
+from genequery.Utilities import RastPy
 
 
 class RastJobDialog(QDialog):
+    _INVALID_INPUT_BORDER = 'border: 1px solid red'
 
     def __init__(self, parent=None):
         super(RastJobDialog, self).__init__(parent)
@@ -13,32 +16,47 @@ class RastJobDialog(QDialog):
         # WIDGETS -----------------------------------------------------------------------
         # user widgets
         userLabel = QLabel('Username')
-        userLineEdit = QLineEdit()
+        self.userLineEdit = QLineEdit()
 
         # password widgets
         passwordLabel = QLabel('Password')
-        passwordLineEdit = QLineEdit()
-        passwordLineEdit.setEchoMode(QLineEdit.Password)
+        self.passwordLineEdit = QLineEdit()
+        self.passwordLineEdit.setEchoMode(QLineEdit.Password)
 
         # option JobID widgets
         jobLabel = QLabel('JobID')
-        jobLineEdit = QLineEdit()
-        jobLineEdit.setPlaceholderText('OPTIONAL - Leave Blank for New Job')
+        self.jobLineEdit = QLineEdit()
+        self.jobLineEdit.setPlaceholderText('OPTIONAL - Leave Blank for New Job')
 
         # buttons
         enterButton = QPushButton('Enter')
+        enterButton.setDefault(True)
+        enterButton.clicked.connect(self.onEnter)
         viewJobsButton = QPushButton('View Jobs')
 
         mainLayout.addWidget(userLabel, 0, 0)
-        mainLayout.addWidget(userLineEdit, 0, 1)
+        mainLayout.addWidget(self.userLineEdit, 0, 1)
         mainLayout.addWidget(passwordLabel, 1, 0)
-        mainLayout.addWidget(passwordLineEdit, 1, 1)
+        mainLayout.addWidget(self.passwordLineEdit, 1, 1)
         mainLayout.addWidget(jobLabel, 2, 0)
-        mainLayout.addWidget(jobLineEdit, 2, 1)
-        mainLayout.addWidget(enterButton, 3, 0)
-        mainLayout.addWidget(viewJobsButton, 3, 1)
+        mainLayout.addWidget(self.jobLineEdit, 2, 1)
+        mainLayout.addWidget(viewJobsButton, 3, 0)
+        mainLayout.addWidget(enterButton, 3, 1)
 
         self.setLayout(mainLayout)
+
+    @pyqtSlot()
+    def onEnter(self):
+        """
+        Slot when the "Enter" button is pressed
+        """
+        # perform check that username and password were entered
+        userInput = self.userLineEdit.text()
+        passInput = self.passwordLineEdit.text()
+        if userInput == '':
+            self.userLineEdit.setStyleSheet(self._INVALID_INPUT_BORDER)
+        if passInput == '':
+            self.passwordLineEdit.setStyleSheet(self._INVALID_INPUT_BORDER)
 
 
 if __name__ == '__main__':
